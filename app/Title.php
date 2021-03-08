d1a7e8806ed9cec0854ef8d8d51f397e4ce600e0
DESKTOP-HHJ9SIF\user <shg99sora@yahoo.co.jp>
2021-03-08 23:14:54 +0900

    称号のDB周りのファイル

---
+++ app/Title.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Title extends Model
+{
+    //
+    //指定するテーブル名を追加
+    protected $table = 'titles';
+
+}
---
+++ app/UserTitle.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UserTitle extends Model
+{
+    //
+    //指定するテーブル名を追加
+    protected $table = 'user_titles';
+}
