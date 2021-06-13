9902d91691adcfa4bfcd22bd9ffaaa8c7a2265af
DESKTOP-HHJ9SIF\user <shg99sora@yahoo.co.jp>
2021-06-13 10:43:54 +0900

    v-forの実装

--- resources/js/views/practice.vue
+++ resources/js/views/practice.vue
@@ -12,12 +12,11 @@
             </v-card-text>
             <v-divider></v-divider>
             <v-card-actions>
-              <v-btn-toggle borderless>
-                <v-layout wrap>
-                  <v-flex xs12 sm6 md3><v-btn class="mb-6 text-caption">羊が何匹いるかは狼には関係なし</v-btn></v-flex>
-                  <v-flex xs12 sm6 md3><v-btn class="mb-6 text-caption">賽（さい）は投げられた</v-btn></v-flex>
-                  <v-flex xs12 sm6 md3><v-btn class="mb-6 text-caption">勇気のあるところに希望あり</v-btn></v-flex>
-                  <v-flex xs12 sm6 md3><v-btn class="mb-6 text-caption">後継者は最強のやつ</v-btn></v-flex>
+              <v-btn-toggle borderless >
+                <v-layout wrap >
+                  <v-flex xs12 sm6 md3 v-for="answer in answers">
+                   <v-btn class="mb-6 text-caption">{{answer.anstertext}}</v-btn>
+                  </v-flex>
                 </v-layout>
               </v-btn-toggle>
             </v-card-actions>
@@ -28,3 +27,19 @@
     </v-container>
   </v-app>
 </template>
+
+
+<script>
+export default {
+  data(){
+    return{
+      answers:[
+          { anstertext:'羊が何匹いるかは狼には関係なし'},
+          { anstertext:'賽（さい）は投げられた'},
+          { anstertext:'勇気のあるところに希望あり'},
+          { anstertext:'後継者は最強のやつ'},
+        ]
+    }
+  }
+}
+</script>
