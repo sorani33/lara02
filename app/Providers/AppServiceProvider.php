b27625d8cc8e627cbb19eabb894a09722e102413
DESKTOP-HHJ9SIF\user <shg99sora@yahoo.co.jp>
2021-06-27 23:25:18 +0900

    heroku用の処理を追加

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        //heroku用の処理
+        if ($this->app->environment() == 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
