120c581705d67d4e8f6470235f231389c762ef25
DESKTOP-HHJ9SIF\user <shg99sora@yahoo.co.jp>
2021-07-01 00:53:33 +0900

    リストクリックしたらmodal表示

---
+++ resources/js/views/@report.vue
@@ -0,0 +1,133 @@
+<template>
+
+  <v-container class="grey lighten-5">
+    <p class="font-weight-black"> 成績板</p>
+	<v-divider></v-divider>
+	<br>
+    <v-row
+      :class="n === 1 ? 'mb-6' : ''"
+      no-gutters
+    >
+      <v-col :key="1">
+        <v-card class="pa-2" outlinedtile shaped >
+		  <v-card-title class="headline">総合スコア　自己ベスト　140</v-card-title>
+		<v-divider class="mx-3"></v-divider>
+		<v-card-text>
+		  <div class="body-1 mb-1">1位　[A]クラウド　300</div>
+		  <div class="body-1 mb-1">2位　[R]ティファ　220</div>
+		  <div class="body-1 mb-1">3位　[F]土生翔吾(はぶっち)＠東京都　3</div>
+		</v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+
+      <v-col :key="2">
+        <v-card class="pa-2" outlinedtile shaped >
+		  <v-card-title class="headline">月間スコア　自己ベスト　140</v-card-title>
+		<v-divider class="mx-3"></v-divider>
+		<v-card-text>
+		  <div class="body-1 mb-1">1位　クラウド　300</div>
+		  <div class="body-1 mb-1">2位　ティファ　220</div>
+		  <div class="body-1 mb-1">3位　バレット　60</div>
+		</v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+
+
+	<br>
+    <p class="text-h1">▼タイムアタック</p>
+    <p class="font-weight-black">▼世界史【2019】</p>
+    <v-row>
+      <v-col>
+        <v-card class="pa-2" outlinedtile shaped >
+		  <v-card-title class="headline">ヨーロッパ史</v-card-title>
+		  <v-card-subtitle>自己ベスト　140</v-card-subtitle>
+		<v-divider class="mx-3"></v-divider>
+		<v-card-text>
+		  <div class="body-1 mb-1">1位　クラウド　300</div>
+		  <div class="body-1 mb-1">2位　ティファ　220</div>
+		  <div class="body-1 mb-1">3位　バレット　60</div>
+		</v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col>
+        <v-card class="pa-2" outlinedtile shaped >
+		  <v-card-title class="headline">中東史</v-card-title>
+		  <v-card-subtitle>自己ベスト　140</v-card-subtitle>
+		<v-divider class="mx-3"></v-divider>
+		<v-card-text>
+		  <div class="body-1 mb-1">1位　クラウド　300</div>
+		  <div class="body-1 mb-1">2位　ティファ　220</div>
+		  <div class="body-1 mb-1">3位　バレット　60</div>
+		</v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col>
+        <v-card class="pa-2" outlinedtile shaped >
+		  <v-card-title class="headline">インド史</v-card-title>
+		  <v-card-subtitle>自己ベスト　140</v-card-subtitle>
+		<v-divider class="mx-3"></v-divider>
+		<v-simple-table dense>
+			<template v-slot:default>
+			<tbody>
+				<tr v-for="item in desserts" :key="item.name">
+				<td>{{ item.name }}</td>
+				<td>{{ item.calories }}</td>
+				</tr>
+			</tbody>
+			</template>
+		</v-simple-table>
+        </v-card>
+      </v-col>
+    </v-row>
+
+
+
+
+  </v-container>
+</template>
+
+
+
+<script>
+export default {
+  data(){
+    return{
+      n:1,
+        desserts: [
+          {
+            name: 'Frozen Yogurt',
+            calories: 159,
+          },
+          {
+            name: 'Ice cream sandwich',
+            calories: 237,
+          },
+          {
+            name: 'Eclair',
+            calories: 262,
+          },
+		],
+
+      titles:{
+        // 1:"例題",
+        // 2:"世界史1　ヨーロッパ史【2019版】",
+        // 3:"世界史2　中東【2019版】",
+        // 4:"世界史3　インド【2019版】",
+        // 5:"世界史4　中国【2019版】",
+        // 6:"世界史5　大航海【2019版】",
+        // 3:"",
+        // 4:"",
+      },
+    }
+  },
+
+}
+</script>
--- resources/js/views/report.vue
+++ resources/js/views/report.vue
@@ -1,109 +1,174 @@
 <template>
+  <v-card
+    class="mx-auto"
+    max-width="600"
+    tile
+  >
+    <v-list flat>
+      <v-subheader>REPORTS</v-subheader>
+      <v-list-item-group
+        v-model="selectedItem"
+        color="primary"
+      >
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+		  @click="selectItem(item)"
+        >
+          <v-list-item-icon>
+            <v-icon v-text="item.icon"></v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.text"></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
 
-  <v-container class="grey lighten-5">
-    <p class="font-weight-black"> 成績板</p>
-	<v-divider></v-divider>
-	<br>
-    <v-row
-      :class="n === 1 ? 'mb-6' : ''"
-      no-gutters
+
+      <v-list-group
+        :value="true"
+        prepend-icon="mdi-account-circle"
+      >
+        <template v-slot:activator>
+          <v-list-item-title>世界史2019</v-list-item-title>
+        </template>
+
+        <v-list-group
+          :value="true"
+          no-action
+          sub-group
+        >
+          <template v-slot:activator>
+            <v-list-item-content>
+              <v-list-item-title>ヨーロッパ史</v-list-item-title>
+            </v-list-item-content>
+          </template>
+
+          <v-list-item
+            v-for="([title, icon], i) in admins"
+            :key="i"
+            link
+          >
+            <v-list-item-title v-text="title"></v-list-item-title>
+
+            <v-list-item-icon>
+              <v-icon v-text="icon"></v-icon>
+            </v-list-item-icon>
+          </v-list-item>
+        </v-list-group>
+
+        <v-list-group
+          no-action
+          sub-group
+        >
+          <template v-slot:activator>
+            <v-list-item-content>
+              <v-list-item-title>中東史</v-list-item-title>
+            </v-list-item-content>
+          </template>
+
+          <v-list-item
+            v-for="([title, icon], i) in cruds"
+            :key="i"
+            link
+          >
+            <v-list-item-title v-text="title"></v-list-item-title>
+
+            <v-list-item-icon>
+              <v-icon v-text="icon"></v-icon>
+            </v-list-item-icon>
+          </v-list-item>
+        </v-list-group>
+      </v-list-group>
+
+
+<v-app id="app">
+  <div class="text-center">
+    <v-dialog
+      v-model="isOpen"
+      width="500"
     >
-      <v-col :key="1">
-        <v-card class="pa-2" outlinedtile shaped >
-		  <v-card-title class="headline">総合スコア　自己ベスト　140</v-card-title>
-		<v-divider class="mx-3"></v-divider>
-		<v-card-text>
-		  <div class="body-1 mb-1">1位　[A]クラウド　300</div>
-		  <div class="body-1 mb-1">2位　[R]ティファ　220</div>
-		  <div class="body-1 mb-1">3位　[F]土生翔吾(はぶっち)＠東京都　3</div>
-		</v-card-text>
-        </v-card>
-      </v-col>
-    </v-row>
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn
+          color="red lighten-2"
+          dark
+          v-bind="attrs"
+          v-on="on"
+        >
+          Click Me
+        </v-btn>
+      </template>
 
-      <v-col :key="2">
-        <v-card class="pa-2" outlinedtile shaped >
-		  <v-card-title class="headline">月間スコア　自己ベスト　140</v-card-title>
-		<v-divider class="mx-3"></v-divider>
-		<v-card-text>
-		  <div class="body-1 mb-1">1位　クラウド　300</div>
-		  <div class="body-1 mb-1">2位　ティファ　220</div>
-		  <div class="body-1 mb-1">3位　バレット　60</div>
-		</v-card-text>
-        </v-card>
-      </v-col>
-    </v-row>
+      <v-card>
+        <v-card-title class="text-h5 grey lighten-2">
+          Privacy Policy
+        </v-card-title>
+
+        <v-card-text>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="primary"
+            text
+            @click="isOpen = false"
+          >
+            I accept
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
+</v-app>
 
 
-	<br>
-    <p class="text-h1">▼タイムアタック</p>
-    <p class="font-weight-black">▼世界史【2019】</p>
-    <v-row>
-      <v-col>
-        <v-card class="pa-2" outlinedtile shaped >
-		  <v-card-title class="headline">ヨーロッパ史</v-card-title>
-		  <v-card-subtitle>自己ベスト　140</v-card-subtitle>
-		<v-divider class="mx-3"></v-divider>
-		<v-card-text>
-		  <div class="body-1 mb-1">1位　クラウド　300</div>
-		  <div class="body-1 mb-1">2位　ティファ　220</div>
-		  <div class="body-1 mb-1">3位　バレット　60</div>
-		</v-card-text>
-        </v-card>
-      </v-col>
-    </v-row>
 
-    <v-row>
-      <v-col>
-        <v-card class="pa-2" outlinedtile shaped >
-		  <v-card-title class="headline">中東史</v-card-title>
-		  <v-card-subtitle>自己ベスト　140</v-card-subtitle>
-		<v-divider class="mx-3"></v-divider>
-		<v-card-text>
-		  <div class="body-1 mb-1">1位　クラウド　300</div>
-		  <div class="body-1 mb-1">2位　ティファ　220</div>
-		  <div class="body-1 mb-1">3位　バレット　60</div>
-		</v-card-text>
-        </v-card>
-      </v-col>
-    </v-row>
 
-    <v-row>
-      <v-col>
-        <v-card class="pa-2" outlinedtile shaped >
-		  <v-card-title class="headline">インド史</v-card-title>
-		  <v-card-subtitle>自己ベスト　140</v-card-subtitle>
-		<v-divider class="mx-3"></v-divider>
-		<v-card-text>
-		  <div class="body-1 mb-1">1位　クラウド　300</div>
-		  <div class="body-1 mb-1">2位　ティファ　220</div>
-		  <div class="body-1 mb-1">3位　バレット　60</div>
-		</v-card-text>
-        </v-card>
-      </v-col>
-    </v-row>
-  </v-container>
+
+
+
+    </v-list>
+  </v-card>
 </template>
 
 
-
 <script>
-export default {
-  data(){
-    return{
-      n:1,
-      titles:{
-        // 1:"例題",
-        // 2:"世界史1　ヨーロッパ史【2019版】",
-        // 3:"世界史2　中東【2019版】",
-        // 4:"世界史3　インド【2019版】",
-        // 5:"世界史4　中国【2019版】",
-        // 6:"世界史5　大航海【2019版】",
-        // 3:"",
-        // 4:"",
-      },
-    }
-  },
+  export default {
+    data: () => ({
+      dialog: false,
+      admins: [
+        ['自己ベスト', 'mdi-account-multiple-outline'],
+        ['ランキング', 'mdi-cog-outline'],
+      ],
+      cruds: [
+        ['Create', 'mdi-plus-outline'],
+        ['Read', 'mdi-file-outline'],
+        ['Update', 'mdi-update'],
+        ['Delete', 'mdi-delete'],
+      ],
 
-}
+      selectedItem: 1,
+      items: [
+        { text: '総合スコア', icon: 'mdi-clock' },
+        { text: '月間スコア', icon: 'mdi-account' },
+      ],
+      item: '',
+      isOpen: false,
+
+
+    }),
+
+  methods: {
+   selectItem(item) {
+	   console.log('月間スコア');
+      this.item = item;
+      this.isOpen = true
+    }
+  }
+
+  }
 </script>
